36ed3060747d5991e0ebc67ca3aeef88902db855
Aniece Thompson <aniecet@yahoo.com>
2019-09-29 05:04:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,7 @@ def monopoly_with_fourth_tier
       },
       :names => {
         :reading_railroad => {
-          "mortgage" => 100
+          "mortgage_value" => 100
           
         },
         :pennsylvania_railroad => {},
